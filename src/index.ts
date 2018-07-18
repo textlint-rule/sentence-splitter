3a7984a5745549d75328800767e9a75714b87139
Adam Schmideg <adam@schmideg.net>
2018-07-18 23:19:21 +0200

    splitToSentences returns string[]

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 "use strict";
 
-import { TxtNode, TxtParentNode } from "@textlint/ast-node-types";
 import { split } from "./sentence-splitter";
 
-export function splitToSentences(text: string): (TxtParentNode | TxtNode)[] {
-    return split(text);
+export function splitToSentences(text: string): string[] {
+    return split(text).map(node => node.raw);
 }
