e8c5783be54239a4722737fd6025b52da4c1c1ba
Adam Schmideg <adam@schmideg.net>
2018-07-20 10:06:10 +0200

    Work with paragraphs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 "use strict";
 
-import { split } from "./sentence-splitter";
+import { split, Syntax } from "./sentence-splitter";
+
+function splitPara(para: string): string[] {
+    return split(para)
+        .filter(node => node.type == Syntax.Sentence)
+        .map(node => node.raw);
+}
 
 export function splitToSentences(text: string): string[] {
-    return split(text).map(node => node.raw);
+    var result = [];
+    for (let para of text.split("\n")) {
+        for (let sentence of splitPara(para)) {
+            result.push(sentence);
+        }
+        result.push("");
+    }
+    result.pop();
+    return result;
 }
