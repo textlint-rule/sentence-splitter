50c96455d3082178582a67c22ce1686d6725d112
Daniel Melo <daniel.melo@ceros.com>
2023-10-23 17:53:35 +0200

    support for using other languages than English

--- src/sentence-splitter.ts
+++ src/sentence-splitter.ts
@@ -7,7 +7,7 @@ import { NewLineParser } from "./parser/NewLineParser.js";
 import { SpaceParser } from "./parser/SpaceParser.js";
 import { DefaultOptions, SeparatorParser, SeparatorParserOptions } from "./parser/SeparatorParser.js";
 import { AnyValueParser } from "./parser/AnyValueParser.js";
-import { AbbrMarker } from "./parser/AbbrMarker.js";
+import { AbbrMarker, AbbrMarkerOptions } from "./parser/AbbrMarker.js";
 import { PairMaker } from "./parser/PairMaker.js";
 import { nodeLog } from "./logger.js";
 
@@ -158,16 +158,17 @@ export { DefaultOptions as DefaultSentenceSplitterOptions };
 
 export interface splitOptions {
     /**
-     * Separator options
+     * Separator & AbbrMarker options
      */
     SeparatorParser?: SeparatorParserOptions;
+    AbbrMarker?: AbbrMarkerOptions;
 }
 
 const createParsers = (options: splitOptions = {}) => {
     const newLine = new NewLineParser();
     const space = new SpaceParser();
     const separator = new SeparatorParser(options.SeparatorParser);
-    const abbrMarker = new AbbrMarker();
+    const abbrMarker = new AbbrMarker(options.AbbrMarker);
     const pairMaker = new PairMaker();
     // anyValueParser has multiple parser and markers.
     // anyValueParse eat any value if it reaches to other value.
